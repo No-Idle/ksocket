dbd3dca5afb284d51244e5227643378ce12392c4
yury <yury.danilevich@mail.ru>
2021-01-23 18:47:23 +0300

    Client id type String -> Int

--- src/server/Server.kt
+++ src/server/Server.kt
@@ -4,27 +4,27 @@ import java.io.IOException
 import java.net.InetAddress
 import java.net.ServerSocket
 import java.net.Socket
+import java.util.concurrent.atomic.AtomicInteger
 
 object Server {
     private const val PORT = 8080
 
-    private var id = 0
+    private var id = AtomicInteger()
 
     @JvmStatic
     fun main(args: Array<String>) {
-        Server.start()
+        start()
     }
 
     private fun start() {
-
         try {
             ServerSocket(PORT).use { server ->
                 println("Server has been started: " + getIpAddress())
-                val sockets = HashMap<String, Socket>()
-                while (true) {
+                val sockets = HashMap<Int, Socket>()
+                repeat(2) {
                     val socket = server.accept()
                     synchronized(sockets) {
-                        sockets[nextId()] = socket
+                        sockets[id.incrementAndGet()] = socket
                     }
                     Thread(ServerExecutor(socket, sockets)).start()
                 }
@@ -34,14 +34,11 @@ object Server {
         }
     }
 
-    fun getIpAddress(): String {
+    private fun getIpAddress(): String {
         val ipAddress = StringBuilder()
         for (b in InetAddress.getLocalHost().address) {
             ipAddress.append((b + 256) % 256).append('.')
         }
         return ipAddress.substring(0, ipAddress.length - 1)
     }
-
-    @Synchronized
-    fun nextId(): String = "${++id}"
 }
--- src/server/ServerExecutor.kt
+++ src/server/ServerExecutor.kt
@@ -3,22 +3,20 @@ package server
 import java.io.*
 import java.net.Socket
 
-class ServerExecutor(private val socket: Socket, private val sockets: MutableMap<String, Socket>) : Runnable {
+class ServerExecutor(private val socket: Socket, private val sockets: MutableMap<Int, Socket>) : Runnable {
     override fun run() {
         try {
             BufferedReader(InputStreamReader(socket.getInputStream())).use { reader ->
                 while (true) {
-                    while (!reader.ready()) {
-                        Thread.sleep(100)
-//                        println("ready: ${reader.ready()}")
-                    }
+                    while (!reader.ready()) Thread.sleep(100)
+
                     val request = reader.readLine()
                     println(request)
                     if (request == "Bye") break
                     val response = getResponse(request)
 
                     synchronized(sockets) {
-                        val diedSockets = HashMap<String, Socket>()
+                        val diedSockets = HashMap<Int, Socket>()
                         sockets.onEach { (id, socket) ->
                             try {
                                 val writer = BufferedWriter(OutputStreamWriter(socket.getOutputStream()))
